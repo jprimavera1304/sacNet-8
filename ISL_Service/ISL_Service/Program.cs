5f0de1e0a789c050da983de1e1b0d288b5dbc387
OakIsland22 <jpsyrelork@gmail.com>
2026-02-05 18:53:22 -0600

    fix cors

--- ISL_Service/ISL_Service/Program.cs
+++ ISL_Service/ISL_Service/Program.cs
@@ -102,8 +102,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("DevCors", policy =>
         policy
             .WithOrigins(
-                "http://127.0.0.1:5500",
-                "http://localhost:5500"
+                "http://127.0.0.1:5501",
+                "http://localhost:5501"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
